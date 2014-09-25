4b7afc6ac92433fb752a248441096f020d4b4e23
wangjun <elexjun@gmail.com>
2014-09-25 15:33:00 +0800

    add plugin-s

--- templates/cpp-template-default/proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ templates/cpp-template-default/proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -27,6 +27,15 @@ THE SOFTWARE.
 package org.cocos2dx.cpp;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.plugin.PluginWrapper;
+import android.os.Bundle;
+
 
 public class AppActivity extends Cocos2dxActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PluginWrapper.init(getContext());
+    }
 }
--- templates/lua-template-default/frameworks/runtime-src/proj.android/src/org/cocos2dx/lua/AppActivity.java
+++ templates/lua-template-default/frameworks/runtime-src/proj.android/src/org/cocos2dx/lua/AppActivity.java
@@ -27,6 +27,14 @@ THE SOFTWARE.
 package org.cocos2dx.lua;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
+import org.cocos2dx.plugin.PluginWrapper;
+import android.os.Bundle;
 
 public class AppActivity extends Cocos2dxActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PluginWrapper.init(getContext());
+    }
 }
